b021b1a22c1deea9459cc734ce27ccc034130c55
NAVEEN KUMAR KOLLI <nkolli@islander.tamucc.edu>
2017-06-23 00:48:29 -0700

    new changes to branch

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ Created on Wed Jun 21 23:27:42 2017
 @author: navin
 """
 #import sys
+"""
 import pandas as pd
 import numpy as np
 import matplotlib as plt
@@ -23,3 +24,25 @@ print (temp1)
 
 print ('\nProbility of getting loan for each Credit History class:\n' )
 print (temp2)
+"""
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+list_x= [1,2,3,5]
+j=1
+list_y=[0]
+for i in range(0,len(list_x)):
+    if(i==0 or i%2 ==0):
+        print(list_x[i])
+        list_y = [].append
+     
+"""for i,index in enumerate(list_x):
+    if(i==0 or i%2 ==0):
+     print(list_x[i])     
+     #print(list_x[j])
+     #j=j+2 """
+     
+        
+        
+    
+    
